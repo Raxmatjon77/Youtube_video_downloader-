26d08a9ef70dd7d741eb2edc6add216a53c33165
raxmatjon <raxmatjonhamidov242@gmail.com>
2023-08-20 14:42:27 +0500

    to deploy render

--- YTVdownloader/settings.py
+++ YTVdownloader/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-$_rkv^1!wui0#nnhlhim=7nd4a@q402ey3)^i7gegkk#99fz1v'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
